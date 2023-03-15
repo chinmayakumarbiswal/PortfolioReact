d29fe5f10cada3d1b600f597cfe9db95227deb03
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2023-03-16 00:54:45 +0530

    get education data from api

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
 import './Education.css'
 
 const Education = () => {
+
+    const [education, setUsers] = useState([])
+
+  const fetchData = () => {
+    fetch("https://chinmayakumarbiswal.in/api/education")
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data.data)
+        console.log(data.data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+
+
+
   return (
     <section class="mh-skills" id="skills">
         <div class="container">
@@ -13,53 +34,19 @@ const Education = () => {
                         </div>
                         <div class="row">
                             <div class="col-lg-4">
+                                {education.length > 0 && (
                                 <ul class="education-box">
-                                    <li>
-                                        <span>2015-2016</span>
-                                        <h6>10th</h6>
-                                        <p>B.N.D.B.P, Jankia</p>
-                                    </li>
-                                    <li>
-                                        <span>2016-2018</span>
-                                        <h6>12th</h6>
-                                        <p>Uniitech Residential College, Nayagarh</p>
-                                    </li>
-                                    <li>
-                                        <span>2018-2021</span>
-                                        <h6>Cloud Technology and Information Security</h6>
-                                        <p>Centurion University of Technology and Management</p>
-                                    </li>
-                                    <li>
-                                        <span>2021</span>
-                                        <h6>Skilled In Odisha</h6>
-                                        <p>Skilled In Odisha Gold Medalist in Cloud Computing</p>
-                                    </li>
-                                    <li>
-                                        <span>2021</span>
-                                        <h6>India Skill 2021</h6>
-                                        <p>India Skills National Representative </p>
-                                    </li>
-                                    <li>
-                                        <span>2021-2023</span>
-                                        <h6>Master in Computer Application</h6>
-                                        <p>Centurion University of Technology and Management</p>
-                                    </li>
-                                    <li>
-                                        <span>2022</span>
-                                        <h6>Web Developer at Mittiland.com</h6>
-                                        <p>Centurion University of Technology and Management</p>
-                                    </li>
-                                    <li>
-                                        <span>2022 - continue</span>
-                                        <h6>Freelance Web Developer</h6>
-                                    </li>
-                                    <li>
-                                        <span>2022-2023</span>
-                                        <h6>Web Developer (Part Time)</h6>
-                                        <p>Centurion University of Technology and Management</p>
-                                    </li>
-                                    
+                                
+                                    {education.map(educationdata => (
+                                        <li>
+                                        <span>{educationdata.year}</span>
+                                        <h6>{educationdata.education}</h6>
+                                        <p>{educationdata.details}</p>
+                                        </li>
+                                    ))}
+                                
                                 </ul>
+                                )}
                             </div>
                         </div>
                     </div>
